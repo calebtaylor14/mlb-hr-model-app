de6e85205a603f170513637dafed7ff4e279e4a8
calebtaylor14 <caleb.taylor14@gmail.com>
2026-06-20 15:14:11 -0400

    Add function to retrieve hitter statistics from CSV

---
+++ statcast.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+def get_hitter_stats(player_name):
+
+    url = "https://baseballsavant.mlb.com/statcast_search/csv?all=true&player_type=batter&year=2026"
+
+    df = pd.read_csv(url)
+
+    # filter player
+    player_data = df[df["player_name"] == player_name]
+
+    if len(player_data) == 0:
+        return {
+            "barrel_pct": 6,
+            "hardhit_pct": 40,
+            "iso": 0.180,
+            "exit_velocity": 88,
+            "pull_air_pct": 18
+        }
+
+    return {
+        "barrel_pct": player_data["barrel"].mean(),
+        "hardhit_pct": player_data["hard_hit_rate"].mean(),
+        "iso": player_data["iso"].mean(),
+        "exit_velocity": player_data["exit_velocity_avg"].mean(),
+        "pull_air_pct": player_data["pull_air_rate"].mean()
+    }
