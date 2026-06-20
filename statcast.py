e2fabc6fd525dc56053559eeefe24fe21dacbdd4
calebtaylor14 <caleb.taylor14@gmail.com>
2026-06-20 15:15:17 -0400

    Add function to retrieve pitcher statistics

--- statcast.py
+++ statcast.py
@@ -26,3 +26,25 @@ def get_hitter_stats(player_name):
         "exit_velocity": player_data["exit_velocity_avg"].mean(),
         "pull_air_pct": player_data["pull_air_rate"].mean()
     }
+def get_pitcher_stats(player_name):
+
+    url = "https://baseballsavant.mlb.com/statcast_search/csv?all=true&player_type=pitcher&year=2026"
+
+    df = pd.read_csv(url)
+
+    player_data = df[df["player_name"] == player_name]
+
+    if len(player_data) == 0:
+        return {
+            "hr9": 1.2,
+            "barrel_allowed": 8,
+            "flyball": 40,
+            "xslg": 0.420
+        }
+
+    return {
+        "hr9": player_data["home_run"].mean(),
+        "barrel_allowed": player_data["barrel"].mean(),
+        "flyball": player_data["launch_angle"].mean(),
+        "xslg": player_data["estimated_slg"].mean()
+    }
