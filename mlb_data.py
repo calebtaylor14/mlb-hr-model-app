ccfc43e6b90522d996f0d0a6374d430b42d0d2e8
calebtaylor14 <caleb.taylor14@gmail.com>
2026-06-20 15:52:09 -0400

    Add function to retrieve probable pitchers from API

--- mlb_data.py
+++ mlb_data.py
@@ -57,3 +57,25 @@ def get_statcast_pitcher_profile(pitcher_name):
         "xslg": 0.430,
         "suppression": 9
     }
+def get_probable_pitchers():
+
+    url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1&hydrate=probablePitcher"
+    res = requests.get(url)
+    data = res.json()
+
+    pitchers = {}
+
+    for d in data.get("dates", []):
+        for g in d.get("games", []):
+
+            game = f"{g['teams']['away']['team']['name']} @ {g['teams']['home']['team']['name']}"
+
+            away_pitcher = g["teams"]["away"].get("probablePitcher", {}).get("fullName")
+            home_pitcher = g["teams"]["home"].get("probablePitcher", {}).get("fullName")
+
+            pitchers[game] = {
+                "away": away_pitcher,
+                "home": home_pitcher
+            }
+
+    return pitchers
